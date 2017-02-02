7154336a21e4c82696ea829ecc2ecc8422db46fa
clauderic <clauderic.demers@shopify.com>
2017-02-02 14:50:36 -0500

    Update demo to use variable heights

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,11 +1,16 @@
 import VirtualizedList from '../../src';
 
-const ROW_HEIGHT = 80;
+const ROW_HEIGHT_POSSIBILITIES = [70, 110, 140, 70, 90, 70];
 const CONTAINER_STYLE = 'width: 400px; height: 600px; overflow-y: auto; border: 1px solid #DDD; margin: 50px auto;';
-const ELEMENT_STYLE = `height: ${ROW_HEIGHT}px; border-bottom: 1px solid #DDD; box-sizing: border-box; display: flex; align-items: center; justify-content: center; font-weight: bold; font-size: 18px; font-family: sans-serif; color: #333;';`;
+const ELEMENT_STYLE = `border-bottom: 1px solid #DDD; box-sizing: border-box; display: flex; align-items: center; justify-content: center; font-weight: bold; font-size: 18px; font-family: sans-serif; color: #333;';`;
 
 // Here, we're generating an array of 1000 numbers [0, 1, 2, 3, 4, ...] to use as our dataset
 const data = Array.from(Array(1000).keys());
+
+function getRandomHeight() {
+  return ROW_HEIGHT_POSSIBILITIES[Math.floor(Math.random()*ROW_HEIGHT_POSSIBILITIES.length)];
+}
+const rowHeights = data.map(getRandomHeight);
 
 (function() {
   // Make sure you have a container to render into
@@ -16,17 +21,17 @@ const data = Array.from(Array(1000).keys());
   // Initialize our VirtualizedList
   var virtualizedList = new VirtualizedList(container, {
     data,
-    rowHeight: ROW_HEIGHT,
-    renderRow: (row) => {
+    rowHeight: rowHeights,
+    renderRow: (row, index) => {
       const element = document.createElement('div');
-      element.setAttribute('style', ELEMENT_STYLE);
+      element.setAttribute('style', `height: ${rowHeights[index]}px; ${ELEMENT_STYLE}`);
       element.innerHTML = row;
 
       return element;
     },
     onMount: () => {
       // We can set the initial index in the onMount callback
-      virtualizedList.scrollToIndex(100);
+      virtualizedList.scrollToIndex(15);
     }
   });
 })();
