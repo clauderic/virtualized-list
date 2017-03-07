86a91eb48201da7f74c06c62360cf09d541a98f2
clauderic <clauderic.demers@shopify.com>
2017-03-07 16:27:12 -0500

    Add jest config

---
+++ babel-jest.config.js
@@ -0,0 +1,8 @@
+const babelJest = require('babel-jest');
+
+module.exports = babelJest.createTransformer({
+  presets: [
+    require('babel-preset-es2015'),
+    require('babel-preset-stage-1')
+  ]
+});
